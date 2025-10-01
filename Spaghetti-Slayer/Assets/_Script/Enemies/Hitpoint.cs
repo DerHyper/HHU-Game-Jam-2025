f4320b6de3a662a278283a399c22304ac33f3e2d
JaessDev <jaess-dev@gmx.de>
2025-10-01 19:34:19 +0200

    deactivate hitpoint

--- Spaghetti-Slayer/Assets/_Script/Enemies/Hitpoint.cs
+++ Spaghetti-Slayer/Assets/_Script/Enemies/Hitpoint.cs
@@ -1,10 +1,24 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hitpoint : MonoBehaviour
 {
+    private bool _isActive = false;
+    public void Deactivate()
+    {
+        _isActive = false;
+        gameObject.SetActive(false);
+    }
+
+    public void Activate()
+    {
+        _isActive = true;
+        gameObject.SetActive(true);
+    }
+
     private void OnMouseDown()
     {
+        if (!_isActive) { return; }
+
         Debug.Log("Hit");
         gameObject.transform.parent.GetComponent<Enemy>().TakeDamage();
     }
