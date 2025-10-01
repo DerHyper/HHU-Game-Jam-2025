7218be6af2e64126122633f8ba6ce3d44923b72a
JaessDev <jaess-dev@gmx.de>
2025-10-01 11:56:51 +0200

    Add the dependency service to retrieve dependencies via it

---
+++ Spaghetti-Slayer/Assets/_Script/DependencyService.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+/// <summary>
+/// A marker interface that simply indicates that a service should be
+/// retrievable via the DependencyService. 
+/// </summary>
+public interface IManager { }
+
+/// <summary>
+/// The dependency service will manage all our dependencies using a list to register
+/// Managers under their interfaces and then retrieving them over a single static method.
+/// 
+/// The object itself will be used as a singleton but access is managed via a single public static method to retrieve any
+/// registered manager interface.
+/// </summary>
+[CreateAssetMenu(fileName = "DependencyManager", menuName = "Systems/Dependency Manager")]
+public class DependencyService : ScriptableObject
+{
+    #region Singleton
+    private static DependencyService Instance { get; set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+
+    [Serializable]
+    private struct DependencyEntry
+    {
+        public UnityEngine.Object implementation;
+    }
+
+    [SerializeField] private List<DependencyEntry> dependencies = new();
+
+    private Dictionary<Type, object> cache;
+
+    public void Start() => Init();
+
+    private void Init()
+    {
+        cache = new Dictionary<Type, object>();
+        foreach (var entry in dependencies)
+        {
+            if (entry.implementation == null) continue;
+
+            var interfaces = entry.implementation.GetType().GetInterfaces();
+            foreach (var iface in interfaces)
+            {
+                if (!cache.ContainsKey(iface))
+                {
+                    cache[iface] = entry.implementation;
+                }
+            }
+        }
+    }
+
+    public static bool TryGet<T>([NotNullWhen(true)] out T result) where T : IManager
+    {
+        if (Instance.cache != null && Instance.cache.TryGetValue(typeof(T), out var value))
+        {
+            result = (T)value;
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+}
